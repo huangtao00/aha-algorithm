74f64a39e390f3ff22f6da9d9725a742e37deae5
huangtao00 <397916230@qq.com>
2018-07-14 20:14:10 +0800

    add bucket sort code

---
+++ bucket-sort.py
@@ -0,0 +1,16 @@
+#!coding:utf8
+import random
+num=random.choices(range(0,11),k=10)  #choose 10 number for 0 to 10 randomly
+
+book=[0 for i in range(11)]
+# print (book)
+for i in num:
+	book[i]+=1
+# print (book)
+result=[]
+for i in range(len(book)):
+	for j in range(book[i]):
+		result.append(i)
+print ("original numbers:{}".format(num))
+print ("sorted numbers:{}".format(result))
+# print result
