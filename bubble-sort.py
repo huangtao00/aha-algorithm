987b029344a524e56817a1e852d799a1d7c15e7f
huangtao00 <397916230@qq.com>
2018-07-14 20:29:01 +0800

    added bubble sort code

---
+++ bubble-sort.py
@@ -0,0 +1,15 @@
+import random
+num=random.choices(range(0,11),k=12)
+welcome="="*14+"\n"+"bubble sort"+"\n"+"="*14
+print (welcome)
+print ("original numbers:{}".format(num))
+for i in range(len(num)):
+	for j in range(0,len(num)-i-1):
+		if num[j]>num[j+1]:
+			temp=num[j]
+			num[j]=num[j+1]
+			num[j+1]=temp
+
+
+print ("sorted numbers:{}".format(num))
+
