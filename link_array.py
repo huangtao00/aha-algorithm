1ef1e7e39dab13521e4553878215301b2f73060c
huangtao00 <397916230@qq.com>
2018-07-16 23:43:04 +0800

    using two array to simulate linked data structure

---
+++ link_array.py
@@ -0,0 +1,59 @@
+#simulation
+data=[3,6,15,19,27,38]
+#    0,1,2,3,4,5
+pos=[1,2,3,4,5,-1]
+print ("before insert:")
+print (data)
+print (pos)
+index=0
+while index!=-1:
+    print (data[index],end=" ")
+    index=pos[index]
+print("\n")
+print ("insert 18")
+cell=18
+index=0
+old=-1
+print("after insert:")
+while index!=-1:
+    # print (index)
+    if cell<data[index]:
+        pos.append(index)
+        pos[old]=len(data)
+        data.append(cell)
+        break
+    old=index
+    index=pos[index]
+print (data)
+print (pos)
+
+index=0
+while index!=-1:
+    print (data[index],end=" ")
+    index=pos[index]
+print ("\n")
+print ("indert 40")
+cell=40
+index=0
+old=-1
+print("after insert:")
+while index!=-1:
+    # print (index)
+    if cell<data[index]:
+        pos.append(index)
+        pos[old]=len(data)
+        data.append(cell)
+        break
+    old=index
+    index=pos[index]
+if index==-1:
+    data.append(cell)
+    pos.append(-1)
+    pos[old]=len(data)-1
+print (data)
+print (pos)
+
+index=0
+while index!=-1:
+    print (data[index],end=" ")
+    index=pos[index]
