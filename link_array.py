0ca3072694e41995b87f82efee6acc62fde7b918
huangtao00 <397916230@qq.com>
2018-07-22 23:05:22 +0800

    update link simulation using array

--- link_array.py
+++ link_array.py
@@ -12,6 +12,26 @@ while index!=-1:
 print("\n")
 print ("insert 18")
 cell=18
+data=[3,6,15,19,27,38]
+#    0,1,2,3,4,5
+pos=[1,2,3,4,5,-1]
+print ("before insert:")
+print (data)
+print (pos)
+index=0
+data=[3,6,15,19,27,38]
+#    0,1,2,3,4,5
+pos=[1,2,3,4,5,-1]
+print ("before insert:")
+print (data)
+print (pos)
+index=0
+while index!=-1:
+    print (data[index],end=" ")
+    index=pos[in
+while index!=-1:
+    print (data[index],end=" ")
+    index=pos[in
 index=0
 old=-1
 print("after insert:")
